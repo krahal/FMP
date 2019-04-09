9e1791f892c6494fb3f26ef89c92f73b616ad9be
Karn Rahal <karn.rahal@alumni.ubc.ca>
2019-04-09 14:08:59 -0700

    Updated route to post and get form data

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const realtor = require('realtorca');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const options = {
-  LongitudeMin: -122.8713893,
-  LongitudeMax: -122.8119087,
-  LatitudeMin: 49.1278206,
-  LatitudeMax: 49.1596561,
-  PriceMin: 700000, // default = 0
-  PriceMax: 1300000,
-  SortBy: 1, // 1 = price, 6 = date posted
-  SortOrder: 'D', // A = ascending, D = descending
-  BedRange: '0-0', // 'min-max'
-  BathRange: '0-0', // 'min-max'
-};
-
-console.log(realtor.buildUrl(options));
+// const options = {
+//   LongitudeMin: -122.8713893,
+//   LongitudeMax: -122.8119087,
+//   LatitudeMin: 49.1278206,
+//   LatitudeMax: 49.1596561,
+//   PriceMin: 700000, // default = 0
+//   PriceMax: 1300000,
+//   SortBy: 1, // 1 = price, 6 = date posted
+//   SortOrder: 'D', // A = ascending, D = descending
+//   BedRange: '0-0', // 'min-max'
+//   BathRange: '0-0', // 'min-max'
+// };
 
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
+  // Get form input
+  const options = {
+    LongitudeMin: parseFloat(req.body.LongitudeMin),
+    LongitudeMax: parseFloat(req.body.LongitudeMax),
+    LatitudeMin: parseFloat(req.body.LatitudeMin),
+    LatitudeMax: parseFloat(req.body.LatitudeMax),
+    PriceMin: parseInt(req.body.PriceMin, 10), // default = 0
+    PriceMax: parseInt(req.body.PriceMax, 10),
+    // SortBy: req.body.SortBy, // 1 = price, 6 = date posted
+    // SortOrder: req.body.SortOrder, // A = ascending, D = descending
+    // BedRange: req.body.BedRange, // 'min-max'
+    // BathRange: req.body.BathRange, // 'min-max'
+  };
+
+  console.log(`Realtor.ca URL is: ${realtor.buildUrl(options)}\n`);
+
+  // Make API call to get house data
   realtor.post(options)
     .then((data) => {
       const prices = [];
       data.Results.forEach((property) => {
-        prices.push(property.Property.Price);
+        const priceStr = property.Property.Price.substring(1);
+        const price = parseInt(priceStr.replace(/,/g, ''), 10);
+        prices.push(price);
       });
       console.log(prices);
       res.status(200).send(`Success from api call.\nThe prices are: ${prices}`);
